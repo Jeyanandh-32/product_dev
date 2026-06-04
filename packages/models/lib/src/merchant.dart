0af0f36664cd7d1fd58b295e940917d697895aaa
Jeyanandh V S <155248243+Jack-Sparrow-7@users.noreply.github.com>
2026-06-04 17:40:25 +0530

    Initialize models package with essential files and structure

---
+++ packages/models/lib/src/merchant.dart
@@ -0,0 +1,15 @@
+class Merchant {
+  final String fullName;
+  final String businessName;
+  final String whatsappNumber;
+  final String email;
+  final String password;
+
+  Merchant({
+    required this.fullName,
+    required this.businessName,
+    required this.whatsappNumber,
+    required this.email,
+    required this.password,
+  });
+}
