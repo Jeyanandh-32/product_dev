2e2e016f3f94afb92f0361f287212465e95c1412
Jeyanandh V S <155248243+Jack-Sparrow-7@users.noreply.github.com>
2026-06-27 18:38:15 +0530

    fix: resolve type inference errors in backend routes middleware

--- backend/routes/_middleware.dart
+++ backend/routes/_middleware.dart
@@ -3,7 +3,11 @@ import 'package:backend/middlewares/provider_middleware.dart';
 import 'package:dart_frog/dart_frog.dart';
 
 Handler middleware(Handler handler) {
-  return handler
+  return ((RequestContext context) async {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    
+    return handler(context);
+  })
       .use(requestLogger())
       .use(corsMiddleware())
       .use(providerMiddleware());
--- terminal/lib/pages/home.dart
+++ terminal/lib/pages/home.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mix/mix.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:terminal/pages/loading.dart';
+import 'package:terminal/providers/categories_provider.dart';
 
 class Home extends ConsumerWidget {
   const Home({super.key});
@@ -10,6 +12,9 @@ class Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ShadTheme.of(context);
+    final categories = ref.watch(categoriesProvider);
+    if (categories.isLoading) return Scaffold(body: Loading());
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -20,6 +25,12 @@ class Home extends ConsumerWidget {
               .fontFamily(GoogleFonts.arizonia().fontFamily!)
               .color(theme.colorScheme.primary),
         ),
+      ),
+      body: RowBox(
+        children: [
+          ColumnBox(),
+          ColumnBox(style: FlexBoxStyler().color(Colors.white)),
+        ],
       ),
     );
   }
